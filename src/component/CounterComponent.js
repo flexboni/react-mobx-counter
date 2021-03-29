8c8af133aaeff6dfe2172d955e03c4f9545d369f
flexboni <kgb8594@gmail.com>
2021-03-29 17:59:17 +0900

    mobX action 적용

--- src/component/CounterComponent.js
+++ src/component/CounterComponent.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { Button, Box } from '@material-ui/core';
 
-import { inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 
 @inject('counterStore')
+@observer
 class CounterComponent extends Component {
   render() {
     const { counterStore } = this.props;
     return (
       <div>
-        <Button variant='contained' color='primary' size='large'>
+        <Button
+          onClick={() => counterStore.decrement()}
+          variant='contained'
+          color='primary'
+          size='large'
+        >
           -
         </Button>
         <Box component='span' m={5}>
           {counterStore._count}
         </Box>
-        <Button variant='contained' color='primary' size='large'>
+        <Button
+          onClick={() => counterStore.increment()}
+          variant='contained'
+          color='primary'
+          size='large'
+        >
           +
         </Button>
       </div>
--- src/store/CounterStore.js
+++ src/store/CounterStore.js
@@ -1,8 +1,28 @@
-import { observable } from 'mobx';
+import { observable, action, makeObservable } from 'mobx';
 
 class CounterStore {
+  constructor() {
+    makeObservable(this);
+  }
+
   @observable
   _count = 5;
+
+  get count() {
+    return this._count;
+  }
+
+  @action
+  increment() {
+    this._count++;
+    console.log(this._count);
+  }
+
+  @action
+  decrement() {
+    this._count--;
+    console.log(this._count);
+  }
 }
 
 export default new CounterStore();
